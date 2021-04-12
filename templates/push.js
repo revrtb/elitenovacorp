bf7fc89de1111b189c9b0858702b19bb3a0d7275
revrtb <admin@revrtb.com>
2021-04-13 03:11:01 +0400

    updatte

--- templates/push.js
+++ templates/push.js
@@ -2,7 +2,7 @@
 	(function(a,d,k,p,u,s,h){
 	    a.adkpush = a.adkpush || function() {(a.adkpush.q=(a.adkpush.q||[])).push(arguments)};
 	    var s=d.getElementsByTagName('head')[0];var q=d.createElement('script');q.async=1;q.src='//static.adkernel.com/webpush/scripts/v1.1/adkwebpush.js';s.appendChild(q);
-	    a.adkpush('init', {host: 'push.EliteNovaCorp.com', channelId: 98, pubKey: 'BKy5FETrOz3jH0twiDt8jPDsG9IBm6uLuNaTdPwyyLzDN8b8vmO_VjwTmMHVI9YpFfPuTTSdhqUH2rhqRccj6xA'});
+	    a.adkpush('init', {host: 'push.elitenovacorp.com', channelId: 98, pubKey: 'BKy5FETrOz3jH0twiDt8jPDsG9IBm6uLuNaTdPwyyLzDN8b8vmO_VjwTmMHVI9YpFfPuTTSdhqUH2rhqRccj6xA'});
 	    a.adkpush('subscribe');
 	})(window, document);
 </script>
